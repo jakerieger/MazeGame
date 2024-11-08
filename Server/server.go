bcd6a240d0e01d1295eb5e0eba2a0b91200eb09a
Jake Rieger <contact.jakerieger@gmail.com>
2024-11-08 00:36:59 -0500

    update server.go

--- Server/server.go
+++ Server/server.go
@@ -5,16 +5,48 @@ import (
 	"net"
 )
 
+const (
+	CmdConnect = iota
+	CmdDisconnect
+	CmdMove
+	CmdRequestPosition
+)
+
+type Client struct {
+	Name  string  `json:"name"`
+	Color uint32  `json:"color"`
+	PosX  float32 `json:"posX"`
+	PosY  float32 `json:"posY"`
+}
+
+type ClientConnectRequest struct {
+	Name  string `json:"name"`
+	Color uint32 `json:"color"`
+}
+
+func RegisterClient(name string, color uint32, positionX, positionY float32) *Client {
+	return &Client{name, color, positionX, positionY}
+}
+
 // { IP : Client{} }
 var connectedClients map[string]*Client
 
 func main() {
+	// Init our client map
+	connectedClients = make(map[string]*Client)
+
 	listener, err := net.Listen("tcp", ":40200")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer listener.Close()
+	defer func(listener net.Listener) {
+		err := listener.Close()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}(listener)
 
 	fmt.Println("Listening on port 40200")
 
@@ -31,18 +63,43 @@ func main() {
 }
 
 func handleClient(conn net.Conn) {
-	defer conn.Close()
+	defer func(conn net.Conn) {
+		err := conn.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(conn)
 
 	// TODO: Later on, this can be optimized once we know the maximum size of a message block
 	buffer := make([]byte, 1024)
 
 	for {
+		// The first 4 bytes of the message should contain the command opcode,
+		// followed by the json body of the request
 		n, err := conn.Read(buffer)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 
-		fmt.Printf("Received: %s", buffer[:n])
+		opcode := int(buffer[0])
+		fmt.Println(opcode)
+
+		jsonBody := string(buffer[4:n])
+		fmt.Println(jsonBody)
+
+		if opcode == CmdConnect {
+			client := RegisterClient("Jake", 0xFFFF0000, 0, 0)
+			connectedClients[conn.RemoteAddr().String()] = client
+		}
+
+		if opcode == CmdDisconnect {
+		}
+
+		if opcode == CmdMove {
+		}
+
+		if opcode == CmdRequestPosition {
+		}
 	}
 }
